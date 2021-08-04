a8d3bf389ee0df94f4473b353e17bb60fc9b4ea6
msrecec <67606889+msrecec@users.noreply.github.com>
2021-08-04 18:32:40 +0200

    added custom name for inputs

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -13,7 +13,7 @@ import {
 })
 export class BetterHighlightDirective implements OnInit {
   @Input() defaultColor: string = "transparent";
-  @Input() highlightColor: string = "blue";
+  @Input("appBetterHighlight") highlightColor: string = "blue";
 
   /**
    * On the element this reference sits access the style property and subproperty
