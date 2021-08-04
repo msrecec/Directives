8ab612e29bfd3a88b47df1224c4dd83371e3fea9
msrecec <67606889+msrecec@users.noreply.github.com>
2021-08-04 18:09:25 +0200

    added reactive directive

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,16 +1,50 @@
-import { Directive, ElementRef, Renderer2 } from "@angular/core";
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from "@angular/core";
 
 @Directive({
   selector: "[appBetterHighlight]",
 })
-export class BetterHighlightDirective {
+export class BetterHighlightDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   "background-color",
+    //   "blue"
+    // );
+  }
+
+  /**
+   * Binds this Directive to a javascript event
+   *
+   * @param eventData
+   */
+
+  @HostListener("mouseenter") mouseover(eventData: Event) {
     this.renderer.setStyle(
       this.elRef.nativeElement,
       "background-color",
       "blue"
+    );
+  }
+
+  /**
+   * Both of these Directives are reactive directives
+   *
+   * @param eventData
+   */
+
+  @HostListener("mouseleave") mouseleave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      "background-color",
+      "transparent"
     );
   }
 }
